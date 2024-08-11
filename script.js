b2e1c6f27d5d85cbdefd68ab5720bc781fb81472
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-08-11 13:23:26 -0600

    defined function to place marker on gamebaord

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 /* Start with the gameboard */
 function createGameBoard() {
-    const gameBoard = [null, null, null, null, null, null, null, null, null];
+  const gameBoard = [null, null, null, null, null, null, null, null, null];
+
+  //Function to place a marker on the gameboard
+
+  const placeMarker = function (position, marker) {
+    if (gameBoard[position] === null) {
+      gameBoard[position] = marker;
+    }
+    if (gameBoard[position] !== null) {
+      return false;
+      console.log("This position is already taken");
+    }
+  };
+
+  return gameBoard;
+
+  // This game will start as a user vers user game
 }
