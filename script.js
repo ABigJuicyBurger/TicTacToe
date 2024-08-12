37a5e8a27c361bae6f56995970aca26fe1c4e525
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-08-12 08:15:01 -0600

    made life easier by making gameboard look like a 2d board; changed win conditions to satisfy that

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 /* Start with the gameboard */
 function createGameBoard() {
   const gameBoard = [null, null, null, null, null, null, null, null, null];
+  const topLeft = gameBoard[0];
+  const topMiddle = gameBoard[1];
+  const topRight = gameBoard[2];
+  const middleLeft = gameBoard[3];
+  const middleMiddle = gameBoard[4];
+  const middleRight = gameBoard[5];
+  const bottomLeft = gameBoard[6];
+  const bottomMiddle = gameBoard[7];
+  const bottomRight = gameBoard[8];
+
 
   //Function to place a marker on the gameboard
 
@@ -19,14 +29,20 @@ function createGameBoard() {
     // check for horizontal 3 in a row
     if (
         (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) || 
-        (gameBaord[3] === gameBoard[4] && gameBoard[4 === gameBoard[5]]) ||
+        (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) ||
         (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8])
        )
        {
         console.log("There is a winner");
         return true;
        }
+    // check for vertical 3 in a row
+    if (
+        (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6])||
+        (gameBoard)
+    )
   }
+
 
   const getGameBoard = function () {
     return gameBoard;
